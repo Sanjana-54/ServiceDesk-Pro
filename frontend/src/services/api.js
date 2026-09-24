57fa3719e13464fdb18d5747d52264980d0465f4
Sanjana-54 <sanjanagoud054@gmail.com>
2026-09-24 14:30:02 +0530

    Fix production API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://servicedesk-pro-backend.onrender.com",
+  baseURL: "https://servicedesk-pro-backend.onrender.com/api",
   withCredentials: true,
 });
 
