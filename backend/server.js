97ed1d603c2a56f4c625b7260af25f85d4b6dd1e
Sanjana-54 <sanjanagoud054@gmail.com>
2026-09-24 14:24:50 +0530

    Fix CORS for Vercel

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,11 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+      "https://service-desk-pro-beta.vercel.app",
+    ],
     credentials: true,
   })
 );
